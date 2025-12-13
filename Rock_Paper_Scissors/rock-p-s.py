6c80bcdf27fbf319b3109ec3903f6bb921db5248
Ádám <angeloadam@elg-bp.edu.hu>
2025-12-13 23:29:43 +0100

    Almost done (W.I.P)

--- Rock_Paper_Scissors/rock-p-s.py
+++ Rock_Paper_Scissors/rock-p-s.py
@@ -7,18 +7,25 @@ with open("Rock_Paper_Scissors/responses.json", "r", encoding="utf-8") as f:
 def clear():
     os.system("cls" if os.name == "nt" else "clear")
 
-userPont = 0
-compPont = 0
+userPoint = 0
+compPoint = 0
 choiceLIST = ["k", "p", "o"]
 langList = ["hu", "en"]
 lang = "en"
 
 class resp():
-    response_name = responses["name"]
-    response_errors = responses["errors"]["lang_errors"]
-    response_max_points = responses["max_point_limit"]
-    response_max_points_too_low = responses["errors"]["too_low"]
-    response_max_points_too_high = responses["errors"]["too_high"]
+    lang = lang
+    name = responses[lang]["name"]
+    choices = responses[lang]["choices"]
+    choicesRPS = responses[lang]["choicesRPS"]
+    errors = responses[lang]["errors"]["lang_errors"]
+    error_unexpected = responses["undefined"]["errors"]["unexpected"]
+    error_wrong_response = responses[lang]["errors"]["wrong_response"]
+    max_points = responses[lang]["max_point"]["limit"]
+    max_points_too_low = responses[lang]["max_point"]["errors"]["too_low"]
+    max_points_too_high = responses[lang]["max_point"]["errors"]["too_high"]
+    outcome = responses[lang]["outcome"]
+    overall_outcome = responses[lang]["overall_outcome"]
 clear()
 resp()
 
@@ -30,35 +37,38 @@ elif lang.lower() == "english" or lang.lower() == "en":
     lang = "en"
 elif lang not in langList:
     lang="undefined"
-    msg_response = template(resp.response_errors[lang]).substitute(
+    msg_response = template(resp.errors).substitute(
     langList=", ".join(langList)
 )
     clear()
     print(msg_response)
     exit()
 else:
-    print("Unexpected error! Leaving imideately...")
+    print(resp.error_unexpected)
     exit()
 
-msg_response = template(resp.response_errors[lang]).substitute(
-    lang = lang,
-    langList=", ".join(langList)
-)
+def msg_response_func_outer():
+    global msg_response
+    msg_response = template(resp.lang).safe_substitute(
+        lang=lang,
+        langList=", ".join(langList),
+    )
 
+msg_response_func_outer()
 clear()
-print(resp.response_name[lang])
-maxPoint = int(input(resp.response_max_points[lang]))
+print(resp.name)
+maxPoint = int(input(resp.max_points))
 
 if maxPoint <= 0:
-    print(f"\nNot a valid point input!\nYour input: {maxPoint}\nPlease restart the game, and choose a valid positive integer number!")
+    print(resp.max_points_too_low)
     exit()
 elif maxPoint > 100:
-    print(f"\nThe maximum point limit is 100!\nYour input: {maxPoint}\nPlease restart the game, and choose a valid positive integer number under 100!")
+    print(resp.max_points_too_high)
     exit()
 elif maxPoint > 0 and maxPoint <=100:
     pass
 else:
-    print("Unexpected error! Leaving imideately...")
+    print(resp.error_unexpected)
     exit()
 
 class comp:
@@ -78,75 +88,93 @@ class comp:
                 self.irasban = "paper"
             else:
                 self.irasban = "rock"
-
+    
     def game(self):
-        global userPont
-        global compPont
-        global kerdesTovabb
-        
+        global userPoint
+        global compPoint
+        global continueQuestion
+
+        def msg_response_func_inner(self):
+            global msg_response
+            msg_response = template(resp.lang).safe_substitute(
+                lang=lang,
+                langList=", ".join(langList),
+                irasbanUser=irasbanUser,
+                computerChoice=self.irasban,
+                userPont=userPoint,
+                compPont=compPoint
+    )
+
+
+
+
+    
         if user == "k" and self.choice == "o" or user == "p" and self.choice == "k" or user == "o" and self.choice == "p":
+            msg_response_func_inner()
             clear()
-            userPont += 1
-            print(">>> KŐ PAPÍR OLLÓ <<<")
-            print(f"\nGratulálok, ezt a kört megnyerted!\nEzt választottad:      {irasbanUser}\nA gép ezt választotta: {self.irasban}\n\nPontszámod:   {userPont}\nGép pontszáma: {compPont}")
+            userPoint += 1
+            print(resp.name)
+            print(resp.outcome["win"])
         elif user == self.choice:
+            msg_response_func_inner()
             clear()
-            print(">>> KŐ PAPÍR OLLÓ <<<")
-            print(f"\nEz a kör DÖNTETLEN!\nEzt választottad:      {irasbanUser}\nA gép ezt választotta: {self.irasban}\n\nPontszámod:   {userPont}\nGép pontszáma: {compPont}")
+            print(resp.name)
+            print(resp.outcome["draw"])
         else:
+            msg_response_func_inner()
             clear()
-            compPont += 1
-            print(">>> KŐ PAPÍR OLLÓ <<<")
-            print(f"\nSajnálom elvesztetted ezt a kört!\nEzt választottad:      {irasbanUser}\nA gép ezt választotta: {self.irasban}\n\nPontszámod:   {userPont}\nGép pontszáma: {compPont}")
-        kerdesTovabb = input("\nA következő körhöz ENTER!\nKilépéshez >> vége\n")
-        if kerdesTovabb.lower() == "vége" or kerdesTovabb.lower() == "vege" or kerdesTovabb.lower() == "exit":
+            compPoint += 1
+            print(resp.name)
+            print(resp.outcome["lose"])
+        continueQuestion = input(resp.outcome["continueQuestion"])
+        if continueQuestion.lower() == "vége" or continueQuestion.lower() == "vege" or continueQuestion.lower() == "exit":
             clear()
             exit()
         
-while userPont < maxPoint and compPont < maxPoint:
+while userPoint < maxPoint and compPoint < maxPoint:
     clear()
-    print(">>> KŐ PAPÍR OLLÓ <<<")
+    print(resp.name)
 
-    user = input("\nKérlek válassz!\n| Kő (k) | Papír (p) | Olló (o) | Kilépés (vége) |   ")
+    user = input(resp.choices)
 
     while True:
         if not user:
             clear()
-            print(">>> KŐ PAPÍR OLLÓ <<<")
-            user = input("\nKérlek válassz!\n| Kő (k) | Papír (p) | Olló (o) | Kilépés (vége) |   ")
-        elif user.lower() == "papír" or user.lower() == "papir":
+            print(resp.name)
+            user = input(resp.choices)
+        elif user.lower() == "papír" or user.lower() == "papir" or user.lower() == "paper":
             user = "p"
-        elif user.lower() == "kő" or user.lower() == "ko":
+        elif user.lower() == "kő" or user.lower() == "ko" or user.lower() == "r" or user.lower() == "rock":
             user = "k"
-        elif user.lower() == "olló" or user.lower() == "ollo":
+        elif user.lower() == "olló" or user.lower() == "ollo" or user.lower() == "s" or user.lower() == "scissors":
             user = "o"
         elif user.lower() == "vége" or user.lower() == "vege" or user.lower() == "exit":
             clear()
             exit()
         elif user != "p" and user != "k" and user != "o":
             clear()
-            print(">>> KŐ PAPÍR OLLÓ <<<")
-            print(f"\nNem érvényes a megadott válasz!\nVálaszod: {user}")
-            user = input("\nKérlek válassz!\n| Kő (k) | Papír (p) | Olló (o) | Kilépés (vége) |   ")
+            print(resp.name)
+            print(resp.error_wrong_response)
+            user = input(resp.choices)
         else:
             break
 
     if user == "o":
-        irasbanUser = "olló"
+        irasbanUser = resp.choicesRPS["scissors"]
     elif user == "p":
-        irasbanUser = "papír"
+        irasbanUser = resp.choicesRPS["paper"]
     else:
-        irasbanUser = "kő"
+        irasbanUser = resp.choicesRPS["rock"]
 
-    comp()
-    comp().game()
+    computer = comp()
+    computer.game()
 
-if userPont == maxPoint:
+if userPoint == maxPoint:
     clear()
-    print(">>> KŐ PAPÍR OLLÓ <<<")
-    print(f"\nGratulálok, Te voltál az első aki megnyert {maxPoint} kört!\n\n>> Pontszámok:\n   A Te pontszámod:  {userPont}\n   A Gép pontszáma:  {compPont}\n")
+    print(resp.name)
+    print(f"\nGratulálok, Te voltál az első aki megnyert {maxPoint} kört!\n\n>> Pontszámok:\n   A Te pontszámod:  {userPoint}\n   A Gép pontszáma:  {compPoint}\n")
 else:
     clear()
-    print(">>> KŐ PAPÍR OLLÓ <<<")
-    print(f"\nSajnálom, a gép volt az aki elsőnek megnyert {maxPoint} kört!\n\n>> Pontszámok:\n   A Te pontszámod:  {userPont}\n   A Gép pontszáma:  {compPont}\n")
+    print(resp.name)
+    print(f"\nSajnálom, a gép volt az aki elsőnek megnyert {maxPoint} kört!\n\n>> Pontszámok:\n   A Te pontszámod:  {userPoint}\n   A Gép pontszáma:  {compPoint}\n")
  
