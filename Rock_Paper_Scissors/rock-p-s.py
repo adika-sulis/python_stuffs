65f8cd3a22636711999a96d137b94ccf5c946904
Ádám <angeloadam@elg-bp.edu.hu>
2025-12-13 23:39:28 +0100

    W.I.P - backup if something goes south

--- Rock_Paper_Scissors/rock-p-s.py
+++ Rock_Paper_Scissors/rock-p-s.py
@@ -26,6 +26,7 @@ class resp():
     max_points_too_high = responses[lang]["max_point"]["errors"]["too_high"]
     outcome = responses[lang]["outcome"]
     overall_outcome = responses[lang]["overall_outcome"]
+
 clear()
 resp()
 
@@ -38,14 +39,26 @@ elif lang.lower() == "english" or lang.lower() == "en":
 elif lang not in langList:
     lang="undefined"
     msg_response = template(resp.errors).substitute(
-    langList=", ".join(langList)
-)
+        langList=", ".join(langList)
+    )
     clear()
     print(msg_response)
     exit()
 else:
     print(resp.error_unexpected)
     exit()
+
+resp.lang = lang
+resp.name = responses[lang]["name"]
+resp.choices = responses[lang]["choices"]
+resp.choicesRPS = responses[lang]["choicesRPS"]
+resp.errors = responses[lang]["errors"]["lang_errors"]
+resp.error_wrong_response = responses[lang]["errors"]["wrong_response"]
+resp.max_points = responses[lang]["max_point"]["limit"]
+resp.max_points_too_low = responses[lang]["max_point"]["errors"]["too_low"]
+resp.max_points_too_high = responses[lang]["max_point"]["errors"]["too_high"]
+resp.outcome = responses[lang]["outcome"]
+resp.overall_outcome = responses[lang]["overall_outcome"]
 
 def msg_response_func_outer():
     global msg_response
@@ -89,48 +102,42 @@ class comp:
             else:
                 self.irasban = "rock"
     
-    def game(self):
-        global userPoint
-        global compPoint
-        global continueQuestion
+    def game(self, user_choice, irasbanUser):
+        global userPoint, compPoint, continueQuestion
 
-        def msg_response_func_inner(self):
-            global msg_response
-            msg_response = template(resp.lang).safe_substitute(
-                lang=lang,
-                langList=", ".join(langList),
-                irasbanUser=irasbanUser,
-                computerChoice=self.irasban,
-                userPont=userPoint,
-                compPont=compPoint
-    )
+        msg_response = template(resp.lang).safe_substitute(
+            lang=lang,
+            langList=", ".join(langList),
+            irasbanUser=irasbanUser,
+            computerChoice=self.irasban,
+            userPont=userPoint,
+            compPont=compPoint
+)
 
 
 
-
-    
-        if user == "k" and self.choice == "o" or user == "p" and self.choice == "k" or user == "o" and self.choice == "p":
-            msg_response_func_inner()
-            clear()
+        if user_choice == "k" and self.choice == "o" or \
+           user_choice == "p" and self.choice == "k" or \
+           user_choice == "o" and self.choice == "p":
             userPoint += 1
+            clear()
             print(resp.name)
             print(resp.outcome["win"])
-        elif user == self.choice:
-            msg_response_func_inner()
+        elif user_choice == self.choice:
             clear()
             print(resp.name)
             print(resp.outcome["draw"])
         else:
-            msg_response_func_inner()
-            clear()
             compPoint += 1
+            clear()
             print(resp.name)
             print(resp.outcome["lose"])
+
         continueQuestion = input(resp.outcome["continueQuestion"])
-        if continueQuestion.lower() == "vége" or continueQuestion.lower() == "vege" or continueQuestion.lower() == "exit":
+        if continueQuestion.lower() in ["vége","vege","exit"]:
             clear()
             exit()
-        
+
 while userPoint < maxPoint and compPoint < maxPoint:
     clear()
     print(resp.name)
@@ -142,16 +149,16 @@ while userPoint < maxPoint and compPoint < maxPoint:
             clear()
             print(resp.name)
             user = input(resp.choices)
-        elif user.lower() == "papír" or user.lower() == "papir" or user.lower() == "paper":
+        elif user.lower() in ["papír","papir","paper"]:
             user = "p"
-        elif user.lower() == "kő" or user.lower() == "ko" or user.lower() == "r" or user.lower() == "rock":
+        elif user.lower() in ["kő","ko","r","rock"]:
             user = "k"
-        elif user.lower() == "olló" or user.lower() == "ollo" or user.lower() == "s" or user.lower() == "scissors":
+        elif user.lower() in ["olló","ollo","s","scissors"]:
             user = "o"
-        elif user.lower() == "vége" or user.lower() == "vege" or user.lower() == "exit":
+        elif user.lower() in ["vége","vege","exit"]:
             clear()
             exit()
-        elif user != "p" and user != "k" and user != "o":
+        elif user not in ["p","k","o"]:
             clear()
             print(resp.name)
             print(resp.error_wrong_response)
@@ -167,7 +174,7 @@ while userPoint < maxPoint and compPoint < maxPoint:
         irasbanUser = resp.choicesRPS["rock"]
 
     computer = comp()
-    computer.game()
+    computer.game(user, irasbanUser)
 
 if userPoint == maxPoint:
     clear()
@@ -177,4 +184,3 @@ else:
     clear()
     print(resp.name)
     print(f"\nSajnálom, a gép volt az aki elsőnek megnyert {maxPoint} kört!\n\n>> Pontszámok:\n   A Te pontszámod:  {userPoint}\n   A Gép pontszáma:  {compPoint}\n")
- 
