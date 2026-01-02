ae41e2411768059569a2e7c032df2f0e598cfc77
Ádám <angeloadam@elg-bp.edu.hu>
2026-01-02 20:01:49 +0100

    Texas Hold'em Poker (W.I.P) Yeah I'm working on it alright?! xd Sincerely Yours, Adam?

---
+++ Poker/texas-hold-em.py
@@ -0,0 +1,25 @@
+import os, random, time
+def clear():
+    os.system("cls" if os.name=="nt" else "clear")
+
+clear()
+
+cards = [2, 3, 4, 5, 6, 7, 8, 9, 10, "ace", "jack", "queen", "king"]
+card_symbols = ["spades", "hearts", "diamonds", "clubs"]
+card_colors = ["black", "red"]
+
+for _ in range(10):
+    player_card = random.choice(cards)
+    player_card_symbol = random.choice(card_symbols)
+
+    if player_card_symbol in ["spades", "clubs"]:
+        player_card_color = "black"
+    else:
+        player_card_color = "red"
+
+    try:
+        print(f"-------\n{player_card.capitalize()} {player_card_color.capitalize()} {player_card_symbol.capitalize()}")
+        time.sleep(0.5)
+    except AttributeError:
+        print(f"-------\n{player_card} {player_card_color.capitalize()} {player_card_symbol.capitalize()}")
+        time.sleep(0.5)
