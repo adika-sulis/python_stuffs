bda606048d72940641995c3d102bf9cb2eff7eb2
Ádám <angeloadam@elg-bp.edu.hu>
2026-01-03 21:03:12 +0100

    Texas Hold'em (W.I.P) A new approach using classes >> OOP. We'll see how it goes. Sincerely Yours, Adam

--- Poker/texas-hold-em.py
+++ Poker/texas-hold-em.py
@@ -8,18 +8,28 @@ cards = [2, 3, 4, 5, 6, 7, 8, 9, 10, "ace", "jack", "queen", "king"]
 card_symbols = ["spades", "hearts", "diamonds", "clubs"]
 card_colors = ["black", "red"]
 
-for _ in range(10):
-    player_card = random.choice(cards)
-    player_card_symbol = random.choice(card_symbols)
+class player:
+    def __init__(self, card, color, symbol):
+        self.card = card
+        self.color = color
+        self.symbol = symbol
 
-    if player_card_symbol in ["spades", "clubs"]:
-        player_card_color = "black"
-    else:
-        player_card_color = "red"
-
+counter = 0
+for _ in range(2):
     try:
-        print(f"-------\n{player_card.capitalize()} {player_card_color.capitalize()} {player_card_symbol.capitalize()}")
-        time.sleep(0.5)
-    except AttributeError:
-        print(f"-------\n{player_card} {player_card_color.capitalize()} {player_card_symbol.capitalize()}")
-        time.sleep(0.5)
+        card = random.choice(cards)
+        symbol = random.choice(card_symbols)
+        if symbol in ["spades", "clubs"]:
+            color = "black"
+        else:
+            color = "red"
+        if counter == 0:
+            card_one = player(card, symbol, color)
+            counter += 1
+        else:
+            card_two = player(card, symbol, color)               
+    except ValueError:
+        pass
+
+print(f"\n------------------\nELSŐ Kártya: {card_one.card} {card_one.symbol} {card_one.color}\nMÁSODIK Kártya: {card_two.card} {card_two.symbol} {card_two.color}\n------------------\n")
+
