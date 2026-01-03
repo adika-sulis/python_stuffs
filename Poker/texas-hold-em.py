16c70aca875f099339a199f07c0f09748636d23b
Ádám <angeloadam@elg-bp.edu.hu>
2026-01-03 23:27:23 +0100

    Texas Hold'em (W.I.P) >> A lot of time had passed. I've made MASSIVE improvements which I'm proud of. >> I've figured out how to make the code shorter and added functions so as mentioned it is shorter and it's way more appealing. >> It wasn't easy but I figured out how to check the player cards and computer cards and how to compare them. It's already close to a 100 lines (if we deduct the empty lines it's less) and I've only made so the player and the computer gets cards... I hate my life. In this project I already have atleast 4-5 hours worth of work wtf. I mean I am still learning this (for example: OOP).
    With love for You all,
    Adam

--- Poker/texas-hold-em.py
+++ Poker/texas-hold-em.py
@@ -4,9 +4,8 @@ def clear():
 
 clear()
 
-cards = [2, 3, 4, 5, 6, 7, 8, 9, 10, "ace", "jack", "queen", "king"]
+cards = [2, 3, 4, 5, 6, 7, 8, 9, 10, "Ace", "Jack", "Queen", "King"]
 card_symbols = ["Spades", "Hearts", "Diamonds", "Clubs"]
-# card_colors = ["black", "red"]
 
 for i in range(13):
     random.shuffle(cards)
@@ -18,13 +17,12 @@ class card_mechanic:
         self.color = color
         self.symbol = symbol
 
-counter = 0
-counterComp = 0
-
-for _ in range(2):
+for _ in range(1):
     try:
         def get2PlayerCards():
             global card,color,symbol,card_one,card_two, counter
+            counter = 0
+
             for __ in range(2):
                 card = random.choice(cards)
                 symbol = random.choice(card_symbols)
@@ -33,32 +31,62 @@ for _ in range(2):
                 else:
                     color = "Red"
                 if counter == 0:
-                    card_one = card_mechanic(card, symbol, color)
+                    card_one = card_mechanic(card, color, symbol)
                     counter += 1
                 else:
-                    card_two = card_mechanic(card, symbol, color)
+                    card_two = card_mechanic(card, color, symbol)
+
+        def scanPlayerCards():
+            global card,color,symbol,card_one,card_two, counter
+
+            while True:
+                if card_one.card == card_two.card and card_one.symbol ==    card_two.symbol:
+                    get2PlayerCards()
+                else:
+                    break        
+
+        def get2CompCards():   
+            global comp_card,comp_color,comp_symbol,comp_card_one,comp_card_two, counterComp
+
+            counterComp = 0
+
+            for ___ in range(2):
+                comp_card = random.choice(cards)
+                comp_symbol = random.choice(card_symbols)
+                if comp_symbol in ["Spades", "Clubs"]:
+                    comp_color = "Black"
+                else:
+                    comp_color = "Red"
+                if counterComp == 0:
+                    comp_card_one = card_mechanic(comp_card, comp_color, comp_symbol)
+                    counterComp += 1
+                else:
+                    comp_card_two = card_mechanic(comp_card, comp_color,comp_symbol)
+
+        def scanCompCards():
+            global comp_card,comp_color,comp_symbol,comp_card_one,comp_card_two, counterComp
+
+            while True:
+                if comp_card_one.card == comp_card_two.card and comp_card_one.symbol == comp_card_two.symbol:
+                    get2CompCards()
+                else:
+                    break
+        def scanCardsTogether():
+            while True:
+                if comp_card_one.card == card_one.card and comp_card_one.symbol == card_one.symbol or comp_card_two.card == card_two.card and comp_card_two.symbol == card_two.symbol or comp_card_one.card == card_two.card and comp_card_one.symbol == card_two.symbol or comp_card_two.card == card_one.card and comp_card_two.symbol == card_one.symbol:
+                    get2CompCards()
+                    get2PlayerCards()
+                    scanCompCards()
+                    scanPlayerCards()
+                    scanCardsTogether()
+                else:
+                    break
+                    
         get2PlayerCards()
-
-        while True:
-            if card_one.card == card_two.card and card_one.color ==     card_two.color and card_one.symbol == card_two.symbol:
-                print(f"\n------------------\nJÁTÉKOS\n   ELSŐ Kártya: {card_one.card} {card_one.symbol} {card_one.color}\n   MÁSODIK Kártya: {card_two.card} {card_two.symbol} {card_two.color}       \n------------------\n")
-
-                get2PlayerCards()
-            # Ha a játékos két kártyája ELTÉR
-            else:
-                break
-
-        comp_card = random.choice(cards)
-        comp_symbol = random.choice(card_symbols)
-        if comp_symbol in ["Spades", "Clubs"]:
-            comp_color = "Black"
-        else:
-            comp_color = "Red"
-        if counterComp == 0:
-            comp_card_one = card_mechanic(comp_card, comp_symbol, comp_color)
-            counterComp += 1
-        else:
-            comp_card_two = card_mechanic(comp_card, comp_symbol, comp_color)                
+        get2CompCards()
+        scanPlayerCards()
+        scanCompCards()
+        scanCardsTogether()
     except ValueError:
         print('ValueError ig')
 
@@ -68,7 +96,7 @@ except FileExistsError:
     pass
 
 with open(f"{os.path.dirname(__file__)}/tries.txt", "w", encoding="utf-8") as t:
-    t.write(f"\n------------------\nJÁTÉKOS\n   ELSŐ Kártya: {card_one.card} {card_one.symbol} {card_one.color}\n   MÁSODIK Kártya: {card_two.card} {card_two.symbol} {card_two.color}       \n------------------\n")
-    t.write(f"\n------------------\nCOMP\n    ELSŐ Kártya: {comp_card_one.card} {comp_card_one.symbol} {comp_card_one.color}\n    MÁSODIK Kártya: {comp_card_two.card} {comp_card_two.symbol} {comp_card_two.color}\n------------------\n")
+    t.write(f"\n------------------\nPlayer\n   First Card: {card_one.card} {card_one.symbol} {card_one.color}\n   Second Card: {card_two.card} {card_two.symbol} {card_two.color}       \n------------------\n")
+    t.write(f"\n------------------\nComputer\n    First Card: {comp_card_one.card} {comp_card_one.symbol} {comp_card_one.color}\n    Second Card: {comp_card_two.card} {comp_card_two.symbol} {comp_card_two.color}\n------------------\n")
 
 
