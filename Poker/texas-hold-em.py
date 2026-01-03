0c4e397ac028a06fa4916170619c51d352ad7901
Ádám <angeloadam@elg-bp.edu.hu>
2026-01-03 22:25:47 +0100

    Texas Hold'em (W.I.P) As mentioned in the previous commit this is the better version of this thing idk. So far I've added some code so it detects if the player somehow gets the same card twice (I know that -- in reality with only a single deck -- its impossible, but my code allows this. Because I've randomised the card generating and not individually added all 54 cards) When it detects that you've got the same cards it'll just give you 2 new cards, this goes until you have 2 different cards. What is this commit? A dev log?? xd I will NOT be writing ts in '.i-sek', that would be torture probably Sincerely Yours, Adam

--- Poker/texas-hold-em.py
+++ Poker/texas-hold-em.py
@@ -5,62 +5,70 @@ def clear():
 clear()
 
 cards = [2, 3, 4, 5, 6, 7, 8, 9, 10, "ace", "jack", "queen", "king"]
-card_symbols = ["spades", "hearts", "diamonds", "clubs"]
+card_symbols = ["Spades", "Hearts", "Diamonds", "Clubs"]
 # card_colors = ["black", "red"]
 
-class player:
+for i in range(13):
+    random.shuffle(cards)
+    random.shuffle(card_symbols)
+
+class card_mechanic:
     def __init__(self, card, color, symbol):
         self.card = card
         self.color = color
         self.symbol = symbol
 
 counter = 0
+counterComp = 0
+
 for _ in range(2):
     try:
-        card = random.choice(cards)
-        symbol = random.choice(card_symbols)
-        if symbol in ["spades", "clubs"]:
-            color = "black"
-        else:
-            color = "red"
-        if counter == 0:
-            card_one = player(card, symbol, color)
-            counter += 1
-        else:
-            card_two = player(card, symbol, color)               
-    except ValueError:
-        pass
+        def get2PlayerCards():
+            global card,color,symbol,card_one,card_two, counter
+            for __ in range(2):
+                card = random.choice(cards)
+                symbol = random.choice(card_symbols)
+                if symbol in ["Spades", "Clubs"]:
+                    color = "Black"
+                else:
+                    color = "Red"
+                if counter == 0:
+                    card_one = card_mechanic(card, symbol, color)
+                    counter += 1
+                else:
+                    card_two = card_mechanic(card, symbol, color)
+        get2PlayerCards()
 
-class comp:
-    def __init__(self, comp_card, comp_color, comp_symbol):
-        self.comp_card = comp_card
-        self.comp_color = comp_color
-        self.comp_symbol = comp_symbol
+        while True:
+            if card_one.card == card_two.card and card_one.color ==     card_two.color and card_one.symbol == card_two.symbol:
+                print(f"\n------------------\nJÁTÉKOS\n   ELSŐ Kártya: {card_one.card} {card_one.symbol} {card_one.color}\n   MÁSODIK Kártya: {card_two.card} {card_two.symbol} {card_two.color}       \n------------------\n")
 
-counter = 0
-for _ in range(2):
-    try:
+                get2PlayerCards()
+            # Ha a játékos két kártyája ELTÉR
+            else:
+                break
+
         comp_card = random.choice(cards)
         comp_symbol = random.choice(card_symbols)
-        if comp_symbol in ["spades", "clubs"]:
-            comp_color = "black"
+        if comp_symbol in ["Spades", "Clubs"]:
+            comp_color = "Black"
         else:
-            comp_color = "red"
-        if counter == 0:
-            comp_card_one = comp(comp_card, comp_symbol, comp_color)
-            counter += 1
+            comp_color = "Red"
+        if counterComp == 0:
+            comp_card_one = card_mechanic(comp_card, comp_symbol, comp_color)
+            counterComp += 1
         else:
-            comp_card_two = comp(comp_card, comp_symbol, comp_color)               
+            comp_card_two = card_mechanic(comp_card, comp_symbol, comp_color)                
     except ValueError:
-        pass
+        print('ValueError ig')
 
 try:
-    open(f"{os.path.dirname(__file__)}/tries.txt", "w", encoding="utf-8")
-except FileNotFoundError:
-        open(f"{os.path.dirname(__file__)}/tries.txt", "a")
+    open(f"{os.path.dirname(__file__)}/tries.txt", "x", encoding="utf-8")
+except FileExistsError:
+    pass
 
 with open(f"{os.path.dirname(__file__)}/tries.txt", "w", encoding="utf-8") as t:
     t.write(f"\n------------------\nJÁTÉKOS\n   ELSŐ Kártya: {card_one.card} {card_one.symbol} {card_one.color}\n   MÁSODIK Kártya: {card_two.card} {card_two.symbol} {card_two.color}       \n------------------\n")
-    t.write(f"\n------------------\nCOMP\n    ELSŐ Kártya: {comp_card_one.comp_card} {comp_card_one.comp_symbol} {comp_card_one.comp_color}\n    MÁSODIK Kártya: {comp_card_two.comp_card}  {comp_card_two.comp_symbol} {comp_card_two.comp_color}\n------------------\n")
+    t.write(f"\n------------------\nCOMP\n    ELSŐ Kártya: {comp_card_one.card} {comp_card_one.symbol} {comp_card_one.color}\n    MÁSODIK Kártya: {comp_card_two.card} {comp_card_two.symbol} {comp_card_two.color}\n------------------\n")
 
 
