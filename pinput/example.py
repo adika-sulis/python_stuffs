2d2ccfb6f14e655dbede6cba845a8d6bc52d3bc5
Ádám <angeloadam@elg-bp.edu.hu>
2026-06-05 13:12:44 +0200

    Pinput example

---
+++ pinput/example.py
@@ -0,0 +1,16 @@
+import pinput as p
+import os # not necessary
+os.system("cls") # not necessary
+
+x = p.pinput("Enter a number: ", "kg")
+# for instance input is 10 kg
+print(x) # 10
+print(type(x)) # <class 'int'>
+y = p.pinput("Enter a list: ", "list")
+# for instance input is list[1, 2, 3]
+print(y) # [1, 2, 3]
+print(type(y)) # <class 'list'>
+y = p.pinput("What is 1.01 + 2.13: ")
+# for instance input is 3.14
+print(y) # 3.14
+print(type(y)) # <class 'float'>
