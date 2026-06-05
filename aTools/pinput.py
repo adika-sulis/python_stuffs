e0cef094967e6234b063e49ad8dbdbfa8627d919
Ádám <angeloadam@elg-bp.edu.hu>
2026-06-05 19:07:26 +0200

    Progress with more content about the book >> renamed folder

---
+++ aTools/pinput.py
@@ -0,0 +1,26 @@
+def pinput(message:str, unit:str=""):
+    x = input(message)
+    x = x.strip()
+    if x.startswith(unit):
+        x = x.removeprefix(unit)
+    elif x.endswith(unit):
+        x = x.removesuffix(unit)
+    try:
+        x = int(x)
+    except ValueError:
+        try:
+            x = float(x)
+        except ValueError:
+            try:
+                x = list(x)
+                y = []
+                for i in x:
+                    if i != "," and i != "[" and i != "]":
+                        y.append(i)
+                x = y
+            except ValueError:
+                try:
+                    x = dict(x)
+                except ValueError:
+                    pass
+    return x
